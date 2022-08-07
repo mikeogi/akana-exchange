38d7b715db8408aa81b868713f14d372413736e1
mike <mike.glyc@gmail.com>
2022-08-07 21:03:15 +0300

    init project

---
+++ src/components/AppCurrencyInput/AppCurrencyInput.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="AppCurrencyInput">
+    currency input
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'AppCurrencyInput',
+  setup() {
+    
+  },
+})
+</script>
---
+++ src/pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="IndexPage">
+    <h1>Hello</h1>
+    <AppCurrencyInput />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import AppCurrencyInput from "~/components/AppCurrencyInput/AppCurrencyInput.vue";
+
+export default defineComponent({
+  components: {AppCurrencyInput},
+  setup() {
+  },
+})
+</script>
